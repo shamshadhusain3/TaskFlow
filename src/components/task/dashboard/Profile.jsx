f52084e4d79a5927b9725bc72a5e42529a781781
shamshadhusain3 <husainshamshad68@gmail.com>
2024-08-13 11:55:40 +0530

    profile from pc

--- src/components/task/dashboard/Profile.jsx
+++ src/components/task/dashboard/Profile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Profile() {
   return (
-    <div className="p-8">
+    
   <div className="max-w-sm mx-auto bg-card rounded-lg shadow-lg p-6">
-  <div className="flex items-center mb-4">
-    <img className="w-16 h-16 rounded-full mr-4" src="https://placehold.co/64x64" alt="Profile Picture" />
+  <div className="flex items-center mb-4 bg-blue-gradient">
+    <img className="w-16 h-16 rounded-full mr-4" src="images/userImage.png" alt="Profile Picture" />
     <div>
       <h2 className="text-lg font-semibold text-foreground">Profile</h2>
       <button className="bg-secondary text-secondary-foreground hover:bg-secondary/80 text-sm px-2 py-1 rounded">Update Pic</button>
@@ -20,7 +20,7 @@ function Profile() {
   </div>
   <button className="bg-primary text-primary-foreground hover:bg-primary/80 w-full py-2 rounded">Update</button>
 </div>
-  </div>
+
   )
 }
 
