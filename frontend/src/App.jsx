396f1e1f572a93e25c30f2af358597e1f75c9584
shamshadhusain3 <husainshamshad68@gmail.com>
2024-09-06 14:48:03 +0530

    added login functionality and protected/private route

---
+++ frontend/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './components/landingPage/LandingPage';
+import Dashboard from './components/task/dashboard/Dashboard';
+import { AdminDashboard } from './components/adminDashboard/AdminDashboard';
+import Login from './components/forms/user/login/Login';
+import Signup from './components/forms/user/signup/Signup';
+import OAuth2RedirectHandler from './components/oauth2/OAuth2RedirectHandler';
+import NotFound from './common/NotFound';
+import { getCurrentUser } from './util/APIUtils';
+import PrivateRoute from './common/PrivateRoute';
+import Alert from 'react-s-alert';
+import 'react-s-alert/dist/s-alert-default.css';
+import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+
+function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is authenticated by fetching the current user
+    const fetchUser = async () => {
+      try {
+        const user = await getCurrentUser();
+        setAuthenticated(true);
+      } catch (error) {
+        setAuthenticated(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <main className='md:mx-11 xl:mx-40'>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Protected route */}
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRoute authenticated={authenticated}>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+
+        <Route path="/login" element={<Login authenticated={authenticated} />} />
+        <Route path="/signup" element={<Signup authenticated={authenticated} />} />
+        <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Alert stack={{ limit: 3 }} />
+    </main>
+  );
+}
+
+export default App;
